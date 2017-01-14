09435adb2b243e45ab2de5060cc5581834dd392b
oato.o <oat9002@hotmail.com>
2017-01-14 13:51:49 +0700

    Aligned main layout

--- app/Main.js
+++ app/Main.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View , Button} from 'react-native';
-import emo from './pics/emo_white.png';
-import people from './pics/people_white.png';
+import { StyleSheet, View , Text , Image , Button} from 'react-native';
+// import { BlurView, VibrancyView } from 'react-native-blur';
+// import { Container, Content} from 'native-base';
 
 class Main extends React.Component {
   render() {
     return(
-        <View style={styles.container}>
-          <View style={styles.view_title}>
-            <Text style={[styles.title, styles.basefont]}>feeln4</Text>
+      <Image source={require("./pics/bg_croped_blur.png")} style={styles.container}>
+        <View>
+          <Text style={[styles.title, styles.basefont]}>feeln4</Text>
+          <Text style={[styles.second_title, styles.basefont]}>Sentimental & Crowd Flow</Text>
+          <Text style={[styles.second_title, styles.basefont]}>Prediction</Text>
+          <Text style={[styles.second_title, styles.basefont]}>System</Text>
+        </View>
+        <View style={[styles.center, {flexDirection: 'row'}]}>
+          <Image source={require("./pics/people_white.png")}></Image>
+          <Image source={require("./pics/emo_white.png")}></Image>
+        </View>
+        <View style={styles.button}>
+          <View>
+            <Button title="Sentiment"></Button>
           </View>
-          <View style={styles.button}>
-            <View>
-              <Button title="Sentiment"></Button>
-            </View>
-            <View style={{marginTop: 10}}>
-              <Button title="Crowd Flow"></Button>
-            </View>
+          <View style={{marginTop: 10}}>
+            <Button title="Clow Flow"></Button>
           </View>
         </View>
+      </Image>
     );
   }
 }
@@ -26,18 +33,29 @@ class Main extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    resizeMode: 'stretch',
+    width: undefined,
+    height: undefined,
     justifyContent: 'space-between',
-    backgroundColor: '#003748'
+    backgroundColor: 'rgba(0,55,72, 0.3)'
   },
   basefont: {
     fontFamily: 'Open Sans'
   },
   title: {
-    fontSize: 48,
-    textAlign: 'center'
+    marginLeft: 30,
+    marginTop: 50,
+    fontSize: 60,
+    color: 'black'
   },
-  view_title: {
-    justifyContent: 'center'
+  second_title: {
+    marginLeft: 30,
+    fontSize: 24,
+    color: 'black'
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   button: {
     marginBottom: 60,
